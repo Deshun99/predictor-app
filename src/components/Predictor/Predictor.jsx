7a9edfcc8fe73736ef9912fa7154c2a1abde9164
Deshun99 <e0560014@u.nus.edu>
2025-05-10 23:36:42 +0800

    feat: removed sound from all cards

--- src/components/Predictor/Predictor.jsx
+++ src/components/Predictor/Predictor.jsx
@@ -39,11 +39,6 @@ const Predictor = () => {
 
   const versionOptions = ["1"];
 
-  const playSound = () => {
-    const audio = new Audio("/houseburndown.mp3");
-    audio.play();
-  };
-
   // Fetch cards only when a version is selected
   useEffect(() => {
     if (selectedVer) {
@@ -329,7 +324,6 @@ const Predictor = () => {
                 variant="contained"
                 disabled={!pendingVersion}
                 onClick={async () => {
-                  playSound();
                   setSelectedVer(pendingVersion);
                   setSelectedBarcodeDown(pendingBarcodeDown);
 
